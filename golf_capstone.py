4759395b2a32948e477623996aae2789e8fb8aa2
jsiu <jsiu@stanford.edu>
2022-01-21 18:17:36 -0800

    added api, read_data, and clean class

--- golf_capstone.py
+++ golf_capstone.py
@@ -1,19 +1,114 @@
 import requests
 from decouple import config
 import mysql.connector
+import os
+import json
 
-#url='http://api.sportradar.us/beachvolleyball/trial/v2/en/competitors/sr:competitor:1/profile.json?'
-#118926
-# url='http://api.sportradar.us/nba/trial/v7/en/players/ab532a66-9314-4d57-ade7-bb54a70c65ad/profile.json?'
-url='http://api.sportradar.us/nba/trial/v7/en/players/:/profile.json?'
 
-#response=requests.get(url, params={"api_key":config("beach_app")})
-response=requests.get(url, params={"api_key":config("nba_call")})
+url= {'ranking':'https://api.sportradar.us/golf/trial/v3/en/players/wgr/2021/rankings.json?',
+'stat':'https://api.sportradar.us/golf/trial/pga/v3/en/2021/players/statistics.json?',
+'pga_player':'https://api.sportradar.us/golf/trial/pga/v3/en/2021/players/profiles.json?',
+'lpga_player':'https://api.sportradar.us/golf/trial/lpga/v3/en/2021/players/profiles.json?',
+'pga_tournament':'https://api.sportradar.us/golf/trial/pga/v3/en/2021/tournaments/schedule.json?',
+'lpga_tournament':'https://api.sportradar.us/golf/trial/lpga/v3/en/2021/tournaments/schedule.json?'
 
-status=response.status_code
-beach_data=response.json()
-print(status)
-print(beach_data)
+#,'player_profile':'https://api.sportradar.us/golf/trial/v3/en/players/a7041051-eb25-40b9-acb3-dab88cae69c0/profile.json?'
+    } #have to edit id
+
+# if I have multiple methods in each class then it would make sense to put in constructor
+
+class Api:
+    def __init__(self):
+        self.cwd=os.getcwd()
+        self.path="Documents\codingnomads\python_capstone"
+
+    def api(self):
+# often data don't need. class method and static methods used more
+#class methods can have class variables. 
+#staff transformer class maintains state.
+    #figure out how to pass a list of links
+    #passing in the url instead of being hard-coded
+    #having the map be a module for import better for testing. sort of a config file
+    # date add to the files, think about if it is going to be rerunned.
+        for key,value in url.items():
+            self.response=requests.get(value, params={"api_key":config("golf_demo")})
+
+            self.status=self.response.status_code
+            # #how to edit out the url part and then add data to the end part
+            self.file  = open(os.path.join(self.cwd,self.path,f"{key}_data.json"), "w+")
+
+            self.golf_data=self.response.json()
+            json.dump(self.golf_data, self.file)
+
+            # print(status)
+            # # print(_data)
+
+            # #pipe all of the variable names here
+            # #have to store the data here
+            #file.write(f"{golf_data}")
+            self.file.close()
+
+#instance variable for object in a different class
+class Read_Files():
+    def __init__(self):
+        self.cwd=os.getcwd()
+        self.path="Documents\codingnomads\python_capstone"
+
+    def read_files(self):
+        self.file_dict={}
+        for key, value in url.items(): # without the asterisk means just gimme the tuple
+            self.file_path=os.path.join(self.cwd,self.path,f'{key}_data.json')
+#how would I include the date?
+
+            #file=open(file_path, "r", encoding="utf-8")
+            with open(self.file_path) as self.json_file:
+                # data = json.load(json_file)
+            # print(file.read())
+                self.file_dict[f'{key}_data']=json.load(self.json_file)#.rstrip("\n")
+    def get_file_dict(self):
+        return self.file_dict
+
+
+# do you need to separate read, clean, 
+#read_file can return the data
+#get_file_dict
+
+class Clean_Data():
+    def __init__(self):
+        self.rank_dict={}
+        self.rank_list=[]
+
+    def clean_rank_data(self):
+        for element in fd["ranking_data"]["players"]:
+            self.rank_dict["id"]=element["id"]
+            self.rank_dict["first_name"]=element["first_name"]
+            self.rank_dict["last_name"]=element["last_name"]
+            self.rank_dict["country"]=element["country"]
+            self.rank_dict["rank"]=element["rank"]
+            self.rank_list.append(self.rank_dict.copy())
+        print(self.rank_list)
+
+#probably make this into another function def clean data 
+    # for element in file_dict["ranking_data"]["players"]:
+    #     print(element)
+    # print(file_dict["stat_data"])
+    # print(file_dict["pga_player_data"])
+    # print(file_dict["lpga_player_data"])
+    # print(file_dict["pga_tournament_data"])
+    # print(file_dict["lpga_tournament_data"])
+    # print(file_dict["player_profile_data"])
+
+#pull=Api()
+#pull.api()
+
+r=Read_Files()
+r.read_files()
+# fd=r.get_file_dict()
+#getter or accessor
+#argument in method of
+
+
+# c=Clean_Data()
 
 """
 class: database
@@ -42,7 +137,7 @@ function query merge api with my data SQL
 function extract data - fetchall
 
 
-
+#mostly done
 class api
 function pull data player profiles
 function pull data player statistics
@@ -85,7 +180,8 @@ inheritance
 
 
 
-
+#add reference table?
+#Expected should all tables have primary keys 
 
 
 """
