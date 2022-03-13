5ada1af49699ac344507a8dd57891de39999377b
jsiu <jsiu1248@gmail.com>
2022-03-13 11:44:18 -0700

    added more notes for remaining changes

--- python_files/Run_App.py
+++ python_files/Run_App.py
@@ -118,3 +118,10 @@ run=Run_App()
 #maybe allow the user to make the app do different things
 #maybe add a golf hole table so that can analyze the averages later.
 #maybe separate the classes into just the inputs from cli and then class to changing the data
+
+
+#finish compare self
+#avg
+#skip sort
+# figure out improvement more
+#exit survey
