de2726c62ede6db462f743deec9e108959b09ddf
jsiu <jsiu1248@gmail.com>
2022-06-05 10:57:27 -0700

    added comment to file_reader.

--- python_files/File_Reader.py
+++ python_files/File_Reader.py
@@ -6,6 +6,7 @@ from Url import url
 
 """"
 Reading the json files
+NOTE: May have to change self.path depending on where folders are stored.
 """
 class File_Reader():
     def __init__(self):
@@ -18,6 +19,7 @@ class File_Reader():
             self.file_path=os.path.join(self.cwd,self.path,f'{key}_data.json')
 #how would I include the date?
 
+# read json files
             with open(self.file_path) as self.json_file:
                 self.file_dict[f'{key}_data']=json.load(self.json_file)#.rstrip("\n")
     def get_file_dict(self):
